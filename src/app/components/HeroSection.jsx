8ee9ef2f0d8cb8ab0f4806d4f9afe4df4fb54c55
nilu-tudu-4648 <nilunilesh84@gmail.com>
2024-07-16 11:46:15 +0530

    size changes

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h2 className="text-white mb-4 text-4xl sm:text-4xl lg:text-6xl lg:leading-normal font-extrabold">
+          <h1 className="text-white mb-4 text-4xl sm:text-4xl lg:text-6xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, we are
               {/* &apos;m{" "} */}
@@ -32,7 +32,7 @@ const HeroSection = () => {
               speed={50}
               repeat={Infinity}
             />
-          </h2>
+          </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
             voluptuous.
